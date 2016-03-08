785481bdba0ce8a0524a9bfaffedea5c79a80c8e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-03-07 20:18:30 -0600

    migrate businesses

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,4 @@
 class Business < ActiveRecord::Base
-  geocoded_by :full_street_address
-  after_validation :geocode          
+  geocoded_by       :full_street_address
+  after_validation  :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }     
 end
