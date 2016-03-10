752c691a945ea217467d20a13329275f7201449a
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-03-10 12:23:42 -0600

    sample seed, remove validation for address Business

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,7 +1,6 @@
 class Business < ActiveRecord::Base
   belongs_to        :business_type
   validates         :name, presence: true
-  validates         :address, presence: true
   validates         :business_type_id, presence: true
   geocoded_by       :address
   after_validation  :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-BusinessType.create(
-  name:   "retail"
-)
-BusinessType.create(
-  name:   "entertainment"
-)
-BusinessType.create(
-  name:   "restaurant/food"
-)
-BusinessType.create(
-  name:   "office/admin"
-)
-BusinessType.create(
-  name:   "gym/sport"
-)
-BusinessType.create(
-  name:   "medical"
-)
-BusinessType.create(
-  name:   "services"
-)
-BusinessType.create(
-  name:   "travel/hotel"
-)
+retail        = BusinessType.create(name:   "retail")
+entertainment = BusinessType.create(name:   "entertainment")
+restaurant    = BusinessType.create(name:   "restaurant/food")
+office        = BusinessType.create(name:   "office/admin")
+gym           = BusinessType.create(name:   "gym/sport")
+medical       = BusinessType.create(name:   "medical")
+service       = BusinessType.create(name:   "services")
+travel        = BusinessType.create(name:   "travel/hotel")
 
 #apartment complexes?
+
+Business.create({
+  name:             "Freebirds World Burrito",
+  business_type_id: restaurant.id
+})
+# {
+#   name:             "Fuzzy’s Taco Shop",
+#   business_type_id: restaurant.id
+# },
+# {
+#   name:             "Galaxy Café",
+#   business_type_id: restaurant.id
+# },
+# {
+#   name:             "Hillside Farmacy",
+#   business_type_id: restaurant.id
+# },
+# {
+#   name:             "Home Slice Pizza",
+#   business_type_id: restaurant.id
+# })
+# businesses.save
