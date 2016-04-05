dfa3036e4d5a07965ca4a27358f2e67e09b86f83
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-04-04 23:12:02 -0500

    add scopes for boolean bans on Business

--- app/models/business.rb
+++ app/models/business.rb
@@ -4,5 +4,7 @@ class Business < ActiveRecord::Base
   validates         :business_type_id, presence: true
   geocoded_by       :address
   after_validation  :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
-  # add scope restrictions per ordinance
+  scope             :all_guns_banned,   -> { where('conceal_ban= ? AND open_ban= ?', true, true) }
+  scope             :concealed_banned,  -> { where('alcohol51percent= ? OR conceal_ban= ?', true, true) }
+
 end
